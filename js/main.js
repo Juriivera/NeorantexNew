6baa78f40e700109bb79a571d1515e93f5afd6d9
Juri Vgr. <yuravegera@gmail.com>
2024-02-02 15:48:08 +0200

    10

--- js/main.js
+++ js/main.js
@@ -9,11 +9,11 @@ const wow = new WOW({
 
   const numberBlock = document.getElementById("numbers");
   const endPoint = 40;
-  const speed = 5; // скорость, время за которое нужно произвести анимацию в сек
+  const speed = 3; // скорость, время за которое нужно произвести анимацию в сек
   var scores = {
     score1: { score: 0,  end: 56 },
     score2: { score: 0,  end: 100 },
-    score3: { score: 0,  end: 17 }
+    score3: { score: 0,  end: 12 }
   };
   
   window.addEventListener('scroll', function() {
